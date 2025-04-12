0c6360b3ff54560803d4af8640c0736e11f219ef
EmersonFras <emersonfrasure@gmail.com>
2025-04-11 23:05:00 -0400

    Shorten program.cs by outting configurations to Extensions

--- Program.cs
+++ Program.cs
@@ -1,75 +1,21 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using dotenv.net;
-using RPI_API.Data;
-using RPI_API.Services;
+using RPI_API.Extensions;
 using RPI_API.Utils;
+
+DotEnv.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddSingleton<Emitter>();
+builder.Services.AddCustomServices(builder.Configuration);
+builder.Services.AddCustomDbContext(builder.Configuration);
+builder.Services.AddCustomAuthentication(builder.Configuration);
+builder.Services.AddCustomCors();
 
-builder.Services.AddScoped<MessageHandler>();
-
-builder.Services.AddScoped<Receiver>(sp =>
-    new Receiver("update.*",
-        sp.GetRequiredService<MessageHandler>().HandleWeatherMessage
-    ));
-
-builder.Services.AddHostedService<ReceiveUpdates>();
-
-builder.Services.AddHostedService<ReceiveUpdates>();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<DisplayContext>(options =>
-    options.UseSqlite(connectionString));
-
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-DotEnv.Load();
-
-var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");  
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options => 
-{
-    options.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            var accessToken = context.Request.Cookies["jwt_token"];
-            if (!string.IsNullOrEmpty(accessToken))
-            {
-                context.Token = accessToken;
-            }
-            return Task.CompletedTask;
-        }
-    };
-
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = "https://rpi-display.duckdns.org:3000",
-        ValidAudience = "https://rpi-matrix.netlify.app",
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
-    };
-});
 
 builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Configure Kestrel to use a .pfx file for HTTPS
 var pfxPassword = Environment.GetEnvironmentVariable("PFX_PASSWORD");
@@ -88,15 +34,7 @@ if (!string.IsNullOrEmpty(pfxPassword))
     });
 }
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigins", policy =>
-    {
-        policy.WithOrigins("https://rpi-matrix.netlify.app", "http://127.0.0.1:5173")
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
+
 
 
 
---
+++ src/Extension/ServiceCollectionExtensions.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RPI_API.Data;
+using RPI_API.Services;
+using RPI_API.Utils;
+using System.Text;
+using dotenv.net;
+
+namespace RPI_API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCustomServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<Emitter>();
+            services.AddScoped<MessageHandler>();
+
+            services.AddScoped<Receiver>(sp =>
+                new Receiver("update.*",
+                sp.GetRequiredService<MessageHandler>().HandleWeatherMessage
+            ));
+
+            services.AddHostedService<ReceiveUpdates>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<DisplayContext>(options =>
+                options.UseSqlite(connectionString));
+            return services;
+        }
+
+        public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");  
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => 
+            {
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Cookies["jwt_token"];
+                        if (!string.IsNullOrEmpty(accessToken))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "https://rpi-display.duckdns.org:3000",
+                    ValidAudience = "https://rpi-matrix.netlify.app",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                };
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddCustomCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigins", policy =>
+                {
+                    policy.WithOrigins("https://rpi-matrix.netlify.app", "http://127.0.0.1:5173")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
+            return services;
+        }
+    }
+}
